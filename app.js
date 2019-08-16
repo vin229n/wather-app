027d9de28bed683d20f9e05a8615c85acb4681ba
Vinayak Patil <vinayak.p@BRILLIO.COM>
2019-08-16 17:04:23 +0530

    weather app friday

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const request = require('request')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forcast')
 
-const url = "https://api.darksky.net/forecast/522bb5225180bbf1a93af4cb87c980c8/37.8267,-122.4233"
-
-request({ url : url},(error,response) =>{
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+geocode(process.argv[2],(error,data)=>{
+    if(error)
+        return console.log('Error:',error)
+    else{
+        forecast(data.latitude,data.longitude,(error,forecastdata) => {
+            if(error)
+                return console.log('Error: ',error)
+            else{
+                console.log(data.location)
+                console.log(forecastdata)
+            }
+        })
+    }
 })
+
+
+
+
+
